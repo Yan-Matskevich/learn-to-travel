44bb4e466531c9a23432df26415c459f25acab8b
Yan-Matskevich <30726855+Yan-Matskevich@users.noreply.github.com>
2020-02-10 09:42:36 +0300

    Add 404 page, add SourceType select for direction form and fixed Email saving (#10)

--- src/main/java/org/learntotravel/web_ui/controller/HomeController.java
+++ src/main/java/org/learntotravel/web_ui/controller/HomeController.java
@@ -1,4 +1,4 @@
-package org.learntotravel.web_ui.home;
+package org.learntotravel.web_ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
